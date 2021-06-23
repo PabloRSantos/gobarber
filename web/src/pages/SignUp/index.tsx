f1b5101341b7250f166f4a2dba2d83c7cf1e19fe
PabloRSantos <pablorsantos15@gmail.com>
2021-06-23 17:21:27 -0300

    feat/web: signUp page

--- web/src/pages/SignUp/index.tsx
+++ web/src/pages/SignUp/index.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
 import { Container, Content, Background } from './styles';
 import logo from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignIn: React.FC = () => {
+const SignUp: React.FC = () => {
     return (
         <Container>
+            <Background />
+
             <Content>
                 <img src={logo} alt="GoBarber" />
 
                 <form>
-                    <h1>Faça seu logon</h1>
+                    <h1>Faça seu cadastro</h1>
+                    <Input icon={FiUser} name="name" placeholder="E-mail" />
                     <Input icon={FiMail} name="email" placeholder="E-mail" />
                     <Input
                         icon={FiLock}
@@ -20,19 +23,15 @@ const SignIn: React.FC = () => {
                         type="password"
                         placeholder="Senha"
                     />
-                    <Button type="submit">Entrar</Button>
-
-                    <a href="forgot">Esqueci minha senha</a>
+                    <Button type="submit">Cadastrar</Button>
                 </form>
                 <a href="signup">
-                    <FiLogIn />
-                    Criar conta
+                    <FiArrowLeft />
+                    Voltar para logon
                 </a>
             </Content>
-
-            <Background />
         </Container>
     );
 };
 
-export { SignIn };
+export { SignUp };
--- web/src/pages/SignUp/styles.ts
+++ web/src/pages/SignUp/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { shade } from 'polished';
-import signInBackground from '../../assets/sign-in-background.png';
+import signUpBackground from '../../assets/sign-up-background.png';
 
 export const Container = styled.div`
     min-height: 100vh;
@@ -41,7 +41,7 @@ export const Content = styled.div`
     }
 
     > a {
-        color: #ff9000;
+        color: #f4ede8;
         display: flex;
         align-items: center;
         margin-top: 24px;
@@ -60,6 +60,6 @@ export const Content = styled.div`
 
 export const Background = styled.div`
     flex: 1;
-    background: url(${signInBackground}) no-repeat center;
+    background: url(${signUpBackground}) no-repeat center;
     background-size: cover;
 `;
